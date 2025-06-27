e314d60c8234e60825f5191c4a83eceb2cac64b0
jona <jonathanalexanderramirezortega@gmail.com>
2025-06-27 00:24:34 -0600

    ✨ feat(service): User Validar duplicado de email y lanzar ConflictException

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -10,7 +10,7 @@ import { Role } from './entities/role.entity';
 import { CreateUserDto } from './dto/user.dto';
 import { Doctor } from './entities/doctor.entity';
 import { CreateDoctorDto } from './dto/doctor.dto';
-
+import { ConflictException } from '@nestjs/common';
 @Injectable()
 export class UsersService {
   #users: User[];
@@ -63,13 +63,24 @@ export class UsersService {
   async create(createUserDto: CreateUserDto): Promise<User> {
     const { name, email, password, age, role_id } = createUserDto;
 
-    const role = await this.rolesRepository.findOneBy({ id: role_id });
-    if (!role) {
-      throw new Error('Rol no encontrado');
+    // Verificar si el email ya existe
+    const existingUser = await this.usersRepository.findOne({
+      where: { email },
+    });
+    if (existingUser) {
+      throw new ConflictException('El correo electrónico ya está en uso');
     }
 
+    // Verificar si el rol existe
+    const role = await this.rolesRepository.findOneBy({ id: role_id });
+    if (!role) {
+      throw  new ConflictException('Rol no encontrado');
+    }
+
+    // Hashear contraseña
     const hashedPassword = await bcrypt.hash(password, 10);
 
+    // Crear usuario
     const user = this.usersRepository.create({
       name,
       email,
@@ -81,25 +92,23 @@ export class UsersService {
     return this.usersRepository.save(user);
   }
 
- async createDoctor(createDoctorDto: CreateDoctorDto): Promise<Doctor> {
-  const { user_id, specialty, consultation_amount } = createDoctorDto;
+  async createDoctor(createDoctorDto: CreateDoctorDto): Promise<Doctor> {
+    const { user_id, specialty, consultation_amount } = createDoctorDto;
 
-  const user = await this.usersRepository.findOne({
-    where: { id: user_id },
-  });
+    const user = await this.usersRepository.findOne({
+      where: { id: user_id },
+    });
 
-  if (!user) {
-    throw new Error('Usuario no encontrado');
+    if (!user) {
+      throw new ConflictException('Usuario no encontrado');
+    }
+
+    const doctor = this.doctorsRepository.create({
+      specialty,
+      consultation_amount,
+      user,
+    });
+
+    return this.doctorsRepository.save(doctor);
   }
-
-  const doctor = this.doctorsRepository.create({
-    specialty,
-    consultation_amount,
-    user,
-  });
-
-  return this.doctorsRepository.save(doctor);
-}
-
-
 }
