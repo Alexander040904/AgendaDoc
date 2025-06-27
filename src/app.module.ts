8f34cc0b32069e759a3484153581f6d5b36d8d7f
jona <jonathanalexanderramirezortega@gmail.com>
2025-06-26 19:57:21 -0600

    ✨ feat(doctor, role, user): Entities integrar TypeORM y entidadades para usuario

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,35 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   
   imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService]
+})
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Para que ConfigService esté disponible en toda la app
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // Importa el módulo para poder inyectar ConfigService
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [],  // Aquí pones tus entidades
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }), AuthModule, UsersModule
+  ],
 })
 export class AppModule {}
